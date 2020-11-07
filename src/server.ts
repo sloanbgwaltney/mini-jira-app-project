1fd373405d7e08a104d5963de4ff875ac47a3ce1
Sloan <sloangwaltney2@gmail.com>
2020-11-07 17:32:40 -0500

    added routes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express = require('express')
 import {connect} from 'mongoose'
+import { routes } from './routes'
 
 connect(process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true})
     .then(() => {
         const app = express()
+        routes(app)
         app.listen(process.env.PORT, () => console.log('Project microservice running'))
     }).catch(e => console.log(e))

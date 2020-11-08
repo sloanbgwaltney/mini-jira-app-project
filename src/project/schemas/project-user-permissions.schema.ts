54f81de529154111aa97e80fe62b0738b4bc8cf4
Sloan <sloangwaltney2@gmail.com>
2020-11-08 16:57:35 -0500

    added permission

--- src/project/schemas/project-user-permissions.schema.ts
+++ src/project/schemas/project-user-permissions.schema.ts
@@ -1,15 +1,21 @@
 import {Schema} from 'mongoose'
 
 export interface IProjectUserPermission {
-    projectOwner: boolean
+    projectOwner: boolean,
+    updateProject: boolean
 }
 
 export enum IProjectUserPermissionEnum {
-    PROJECT_OWNER = 'projectOwner'
+    PROJECT_OWNER = 'projectOwner',
+    UPDATE_PROJECT = 'updateProject'
 }
 
 export const ProjectUserPermissionSchema = new Schema({
     projectOwner: {
+        type: Boolean,
+        default: false
+    },
+    updateProject: {
         type: Boolean,
         default: false
     }

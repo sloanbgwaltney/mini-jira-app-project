f2dbeff07db7b18c4c5f770498509fae073af2e2
Sloan <sloangwaltney2@gmail.com>
2020-11-08 15:53:04 -0500

    added some methods

--- src/project/project.model.ts
+++ src/project/project.model.ts
@@ -1,10 +1,14 @@
 import {Schema, model, Document, Model} from 'mongoose'
+import { IProjectUserPermissionEnum } from './schemas/project-user-permissions.schema'
 import { IProjectUser, ProjectUserSchema } from './schemas/project-user.schema'
 
 export interface IProject extends Document {
     name: string,
     description: string,
-    users: Map<string, IProjectUser>
+    users: Map<string, IProjectUser>,
+    assignUserPermission: (this: IProject, userId: string, permission: IProjectUserPermissionEnum) => IProject,
+    validateUserPermission: (this: IProject, userId: string, permission: IProjectUserPermissionEnum) => boolean,
+    userIsAProjectOwner: (this: IProject, userId: string) => boolean
 }
 
 export interface IProjectModel extends Model<IProject> {}
@@ -25,6 +29,30 @@ export const ProjectSchema = new Schema({
         of: ProjectUserSchema
     }
 })
+
+
+ProjectSchema.methods.assignUserPermission = function(this: IProject, userId: string, permission: IProjectUserPermissionEnum): IProject {
+    const newProject = this
+    const projectUser = newProject.users.get(userId)
+    if (!projectUser) {
+        const newProjectUser: IProjectUser = {userId, permissions: {[permission]: true}}
+        newProject.set(userId, newProjectUser)
+    } else {
+        projectUser.permissions[permission] == true
+        newProject.users.set(userId, projectUser)
+    }
+    return newProject
+}
+
+ProjectSchema.methods.validateUserPermission = function(this: IProject, userId: string, permission: IProjectUserPermissionEnum): boolean {
+    const user = this.users.get(userId)
+    if (!user) return false
+    return user.permissions[permission]
+}
+
+ProjectSchema.methods.userIsAProjectOwner = function(this: IProject, userId: string): boolean {
+    return this.validateUserPermission(userId, IProjectUserPermissionEnum.PROJECT_OWNER)
+}
 
 export const PROJECT_COLLECTION_NAME = 'Project'
 

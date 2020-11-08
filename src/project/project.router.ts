d358bde386f8edccf7086bc56c3ef0ebfe2e56a6
Sloan <sloangwaltney2@gmail.com>
2020-11-08 17:04:49 -0500

    added passport

--- src/project/project.router.ts
+++ src/project/project.router.ts
@@ -1,11 +1,12 @@
 import {Router} from 'express'
+import passport from 'passport'
 import { createOrUpdateProjectController, createProjectController, deleteProjectController, updateExistingProjectController } from './project.controller'
 import { Project } from './project.model'
 import { createOrUpdateProjectService, createProjectService, deleteProjectService, updateExistingProjectService } from './project.service'
 
 export const ProjectRouter = Router()
-.post('/', createProjectController(createProjectService(Project)))
+.post('/', passport.authenticate('jwt', {session: false}), createProjectController(createProjectService(Project)))
 // TODO: see if we can make this line a little neater without the cost of performance
-.put('/', createOrUpdateProjectController(createOrUpdateProjectService(updateExistingProjectService(Project), createProjectService(Project))))
-.patch('/', updateExistingProjectController(updateExistingProjectService(Project)))
-.delete('/', deleteProjectController(deleteProjectService(Project)))
+.put('/', passport.authenticate('jwt', {session: false}), createOrUpdateProjectController(createOrUpdateProjectService(updateExistingProjectService(Project), createProjectService(Project))))
+.patch('/', passport.authenticate('jwt', {session: false}), updateExistingProjectController(updateExistingProjectService(Project)))
+.delete('/', passport.authenticate('jwt', {session: false}), deleteProjectController(deleteProjectService(Project)))
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express = require('express')
 import {connect} from 'mongoose'
 import { routes } from './routes'
+import passport from 'passport'
+import { configPassport } from './config/passport'
 
 connect(process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true})
     .then(() => {
         const app = express()
+        app.use(passport.initialize())
+        configPassport(passport)
         routes(app)
         app.listen(process.env.PORT, () => console.log('Project microservice running'))
     }).catch(e => console.log(e))

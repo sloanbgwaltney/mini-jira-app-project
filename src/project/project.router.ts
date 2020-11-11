2ae7cc99c2e0557993ddb7aa05bba71285e08e95
Sloan <sloangwaltney2@gmail.com>
2020-11-11 16:33:59 -0500

    project router works

--- src/project/project.router.ts
+++ src/project/project.router.ts
@@ -1,12 +1,53 @@
 import {Router} from 'express'
 import passport = require('passport')
-import { createOrUpdateProjectController, createProjectController, deleteProjectController, updateExistingProjectController } from './project.controller'
+import { responseSender } from '../lib/response-sender'
+import { setContextFromRequest } from '../lib/set-context-from-request'
 import { Project } from './project.model'
-import { createOrUpdateProjectService, createProjectService, deleteProjectService, updateExistingProjectService } from './project.service'
+import { IProjectUserPermissionEnum } from './schemas/project-user-permissions.schema'
 
 export const ProjectRouter = Router()
-.post('/', passport.authenticate('jwt', {session: false}), createProjectController(createProjectService(Project)))
-// TODO: see if we can make this line a little neater without the cost of performance
-.put('/', passport.authenticate('jwt', {session: false}), createOrUpdateProjectController(createOrUpdateProjectService(updateExistingProjectService(Project), createProjectService(Project))))
-.patch('/', passport.authenticate('jwt', {session: false}), updateExistingProjectController(updateExistingProjectService(Project)))
-.delete('/', passport.authenticate('jwt', {session: false}), deleteProjectController(deleteProjectService(Project)))
+.post(
+    '/', 
+    passport.authenticate('jwt', {session: false}), 
+    Project.modelFromBody('project'),
+    setContextFromRequest('user', 'sub', 'userId'),
+    Project.assignUserPermission('project', 'userId', IProjectUserPermissionEnum.PROJECT_OWNER),
+    Project.saveEntity('project', 'project'),
+    responseSender(201, 'project')
+)
+.get(
+    '/',
+    passport.authenticate('jwt', {session: false}),
+    setContextFromRequest('user', 'sub', 'userId'),
+    Project.findCurrentUsersProjects('projects'),
+    responseSender(200, 'projects')
+)
+.get(
+    '/:projectId',
+    passport.authenticate('jwt', {session: false}),
+    setContextFromRequest('params', 'projectId', 'projectId'),
+    setContextFromRequest('user', 'sub', 'userId'),
+    Project.findEntityById('projectId', 'project'),
+    Project.userHasPermission('userId', 'project'),
+    responseSender(200, 'project')
+)
+.patch(
+    '/:projectId', 
+    passport.authenticate('jwt', {session: false}), 
+    setContextFromRequest('params', 'projectId', 'projectId'),
+    setContextFromRequest('user', 'sub', 'userId'),
+    Project.findEntityById('projectId', 'project'),
+    Project.userHasPermission('userId', 'project', [IProjectUserPermissionEnum.PROJECT_OWNER, IProjectUserPermissionEnum.UPDATE_PROJECT]),
+    Project.updateEntityById('projectId', 'project'),
+    responseSender(200)
+)
+.delete(
+    '/:projectId',
+    passport.authenticate('jwt', {session: false}),
+    setContextFromRequest('params', 'projectId', 'projectId'),
+    setContextFromRequest('user', 'sub', 'userId'),
+    Project.findEntityById('projectId', 'project'),
+    Project.userHasPermission('userId', 'project', [IProjectUserPermissionEnum.PROJECT_OWNER]),
+    Project.deleteEntityById('project'),
+    responseSender(200)
+)

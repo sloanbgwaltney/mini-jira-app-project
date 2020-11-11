915b0bd6ca94fc1327d33cce7ad4f771a3f1a1e2
Sloan <sloangwaltney2@gmail.com>
2020-11-11 16:33:19 -0500

    added context initializer

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,14 @@ import {connect} from 'mongoose'
 import { routes } from './routes'
 import passport = require('passport')
 import { configPassport } from './config/passport'
+import { requestContextInitializer } from './lib/request-context-initializer'
 
 connect(process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true})
     .then(() => {
         const app = express()
         app.use(express.json())
         app.use(passport.initialize())
+        app.use(requestContextInitializer())
         configPassport(passport)
         routes(app)
         app.listen(process.env.PORT, () => console.log('Project microservice running'))
